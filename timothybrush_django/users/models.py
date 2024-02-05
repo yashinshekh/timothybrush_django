54ddb3b1706d8a77c770e1676a38b7aed1b19dcf
yashinshekh <developeryasin@gmail.com>
2024-02-06 00:25:53 +0545

    asf

--- timothybrush_django/users/models.py
+++ timothybrush_django/users/models.py
@@ -8,11 +8,26 @@ from timothybrush_django.users.managers import UserManager
 
 
 class User(AbstractUser):
+
+    PROVINCE_CHOICES = [
+        ('', 'Select Province/State'),  # Add an empty option if needed
+        ('AB', 'Alberta'),
+        ('BC', 'British Columbia'),
+        ('MB', 'Manitoba'),
+        ('NB', 'New Brunswick'),
+        ('NL', 'Newfoundland and Labrador'),
+        ('NS', 'Nova Scotia'),
+        ('ON', 'Ontario'),
+        ('PE', 'Prince Edward Island'),
+        ('QC', 'Quebec'),
+        ('SK', 'Saskatchewan'),
+    ]
+
     email = models.EmailField(_("email address"), unique=True)
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     street_address = models.CharField(_("Street Address"), max_length=255,default='')
     city = models.CharField(_("City"), max_length=100,default='')
-    province_state = models.CharField(_("Province/State"), max_length=2,default='')
+    province_state = models.CharField(_("Province/State"), max_length=2, choices=PROVINCE_CHOICES, default='')
     postal_code = models.CharField(_("Postal/Zip Code"), max_length=20,default='')
 
     USERNAME_FIELD = "email"
