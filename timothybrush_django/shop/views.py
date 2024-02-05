f701c3d8e767d642d38f3693ba133f99a60cace7
yashinshekh <developeryasin@gmail.com>
2024-02-06 00:34:28 +0545

    modified shop form

--- timothybrush_django/shop/views.py
+++ timothybrush_django/shop/views.py
@@ -4,7 +4,4 @@ from .models import Product
 
 def shop(request):
     products = Product.objects.all()
-    if products:
-        return render(request, 'shop/home.html', {'products': products})
-    else:
-        return HttpResponse('<h1>No product available</h1>')
+    return render(request, 'shop/home.html', {'products': products})
