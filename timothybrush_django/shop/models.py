8bb3e7f8c3adaee6b5f0bb3990d7fe6710569246
yashinshekh <developeryasin@gmail.com>
2024-02-05 17:08:43 +0545

    added category and other fields in model

--- timothybrush_django/shop/models.py
+++ timothybrush_django/shop/models.py
@@ -2,9 +2,61 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Product(models.Model):
+    MALE = 'male'
+    FEMALE = 'female'
+    OTHER = 'other'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+
+    TSHIRT = 'tshirt'
+    CAP = 'cap'
+    TOQUE = 'toque'
+
+    CATEGORY_CHOICES = [
+        (TSHIRT, 'T-shirts'),
+        (CAP, 'Baseball Caps'),
+        (TOQUE, 'Toques'),
+    ]
+
+    SMALL = 'S'
+    MEDIUM = 'M'
+    LARGE = 'L'
+    XLARGE = 'XL'
+    XXLARGE = 'XXL'
+
+    SIZE_CHOICES = [
+        (SMALL, 'S'),
+        (MEDIUM, 'M'),
+        (LARGE, 'L'),
+        (XLARGE, 'XL'),
+        (XXLARGE, 'XXL'),
+    ]
+
+    RED = 'red'
+    BLUE = 'blue'
+    GREEN = 'green'
+    YELLOW = 'yellow'
+    BLACK = 'black'
+
+    COLOR_CHOICES = [
+        (RED, 'Red'),
+        (BLUE, 'Blue'),
+        (GREEN, 'Green'),
+        (YELLOW, 'Yellow'),
+        (BLACK, 'Black'),
+    ]
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=3, choices=SIZE_CHOICES, blank=True, null=True)
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES, blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default=OTHER)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default=TSHIRT)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
 
     def __str__(self):
